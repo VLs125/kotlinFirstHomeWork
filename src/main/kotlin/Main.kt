a5886e9a38c642e0f99ccc7fb76520c38a094db9
Vladislav S <>
2024-11-22 20:54:36 +0300

    Задача №1. Денежные переводы

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package org.netology_exceptions
 
 fun main() {
-    println("Hello World!")
+    commisionCalculate(1000)
+    commisionCalculate(100000)
+    commisionCalculate(100)
+}
+
+fun commisionCalculate(transferAmount: Int) {
+    val baseCommision = 35
+    val basePercent = 0.75
+    var calcCommisionFromTransfer = (transferAmount * basePercent) / 100
+    var amount = if (calcCommisionFromTransfer < baseCommision) baseCommision else calcCommisionFromTransfer
+
+    println(amount)
 }
